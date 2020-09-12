b6896e94d067022a03c933332dd47374946ee241
mirageN1349 <desertirpipi@gmail.com>
2020-09-12 15:34:10 +0300

    fix build

---
+++ build/precache-manifest.37fd0f8d04de056a20569e9c9ae227e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4130977e4e868dffed2b75c198936a71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c28f63b409696ef70ae",
+    "url": "/static/css/main.c40fc8c7.chunk.css"
+  },
+  {
+    "revision": "02fb0da88b53c3f19b58",
+    "url": "/static/js/2.8eb62c76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8eb62c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c28f63b409696ef70ae",
+    "url": "/static/js/main.d3a24d55.chunk.js"
+  },
+  {
+    "revision": "bdee7a3a46be79e61ac0",
+    "url": "/static/js/runtime-main.6629937f.js"
+  }
+]);
